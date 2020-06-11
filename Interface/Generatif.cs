77a6f56c4e2319c5f8dcdec2ed3b500174dcadc3
SNuryahya <stya58@gmail.com>
2020-06-11 23:39:09 +0700

    OOP Bagian Terahir

---
+++ Interface/Generatif.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Abtraction.Interface
+{
+    public class Generatif : IMacamMacam
+    {
+        public void MacamMacam()
+        {
+            Console.WriteLine("Robusta adalah Jenis kopi yang memiliki bentuk bulat dan rasa yang pahit");
+        }
+    }
+}
